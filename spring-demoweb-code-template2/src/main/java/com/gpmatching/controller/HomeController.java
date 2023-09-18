475124ce709b86baafe4270b047dd9f0b9ba3fae
willy <willy@example.com>
2023-09-18 16:48:06 +0900

    test

---
+++ spring-demoweb-code-template2/src/main/java/com/gpmatching/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.gpmatching.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(path = { "/", "/home" })
+	public String home() {
+		
+		return "home";
+	}
+	
+}
