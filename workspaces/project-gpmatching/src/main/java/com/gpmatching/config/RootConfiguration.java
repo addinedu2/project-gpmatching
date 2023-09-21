4eb8c19237a5b78400f0f0fd40cfd56c3db04f69
iriverlee <iriverlee@example.com>
2023-09-21 09:57:42 +0900

    shared database

--- workspaces/project-gpmatching/src/main/java/com/gpmatching/config/RootConfiguration.java
+++ workspaces/project-gpmatching/src/main/java/com/gpmatching/config/RootConfiguration.java
@@ -43,7 +43,8 @@ public class RootConfiguration implements ApplicationContextAware {
 		
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/gpmatchingdb" );
+		//dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/gpmatchingdb" );
+		dataSource.setUrl("jdbc:mysql://3.39.195.155/gpmatchingdb" );
 		dataSource.setUsername("gpmatching");
 		dataSource.setPassword("gpmatching");
 		dataSource.setMaxTotal(10);
