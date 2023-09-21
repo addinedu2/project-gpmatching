2885c5b41bce7691dca8382814c3ea5b306da43a
jh <jh@example.com>
2023-09-21 09:58:27 +0900

    text

---
+++ workspaces/project-gpmatching/src/main/java/com/gpmatching/controller/MyPageController.java
@@ -0,0 +1,10 @@
+package com.gpmatching.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = {"/account"})
+public class MyPageController {
+
+}
