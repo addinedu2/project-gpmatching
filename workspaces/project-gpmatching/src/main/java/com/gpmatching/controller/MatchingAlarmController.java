63cb5fe382b6871d7fcf600bf3b66aa3e3121b63
willy <willy@example.com>
2023-10-04 16:22:43 +0900

    push를 받기 위한 commit

--- workspaces/project-gpmatching/src/main/java/com/gpmatching/controller/MatchingAlarmController.java
+++ workspaces/project-gpmatching/src/main/java/com/gpmatching/controller/MatchingAlarmController.java
@@ -2,7 +2,12 @@ package com.gpmatching.controller;
 
 import org.springframework.stereotype.Controller;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class MatchingAlarmController {
+
+	
 
 }
